76e3341a9c0ce23b3c78fdc05b485061fc93c2df
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2022-08-09 14:20:43 +0900

    Add files via upload

---
+++ kbsc/src/main/java/kbsc/kbsc/KbscApplication.java
@@ -0,0 +1,13 @@
+package kbsc.kbsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbscApplication.class, args);
+	}
+
+}
