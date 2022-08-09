ffea6a6acf1eecfbd96d74034cbd6baaa36f2350
SoohyeonB <susu19@ewhain.net>
2022-08-09 14:29:24 +0900

    push test

--- src/main/java/kbsc/kbsc/KbscApplication.java
+++ src/main/java/kbsc/kbsc/KbscApplication.java
@@ -2,7 +2,7 @@ package kbsc.kbsc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//commit test
 @SpringBootApplication
 public class KbscApplication {
 
