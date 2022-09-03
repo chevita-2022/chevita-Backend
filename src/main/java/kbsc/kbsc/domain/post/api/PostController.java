8bec4d3808fbcfa7d592fc1721a6080850ab9276
SoohyeonB <susu19@ewhain.net>
2022-09-03 17:01:39 +0900

    [CHORE] Build directory structure

---
+++ src/main/java/kbsc/kbsc/domain/post/api/PostController.java
@@ -0,0 +1,12 @@
+package kbsc.kbsc.domain.post.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/posts")
+@RestController
+public class PostController {
+
+    public PostController() {
+    }
+}
