1379334fb848f3b35026b4cfe949b5cd98cfe65a
Haeun-Y <hahaha329@ewhain.net>
2022-09-13 01:48:45 +0900

    [FEAT] user 프로필 이미지 지정

--- src/main/java/kbsc/kbsc/domain/user/dao/UserDAO.java
+++ src/main/java/kbsc/kbsc/domain/user/dao/UserDAO.java
@@ -3,12 +3,13 @@ package kbsc.kbsc.domain.user.dao;
 import kbsc.kbsc.domain.post.domain.Post;
 import kbsc.kbsc.domain.user.domain.Users;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserDAO {
 
-    Users saveUser(Users userEntity);
+    Users saveUser(Users userEntity) throws IOException;
     Users findUserByIdx(Long userIdx);
     Optional<Users> updateInfo(Long targetIdx, Users user);
     //List<Post> findNanumHistory(Long userIdx);
