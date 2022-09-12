69ff98bb1ae58ee83b6238bd09227792f2464c69
Haeun-Y <hahaha329@ewhain.net>
2022-09-12 14:56:13 +0900

    [FEAT] chat 구조 수정

---
+++ src/main/java/kbsc/kbsc/domain/chat/Repository/ChatMessageRepository.java
@@ -0,0 +1,9 @@
+package kbsc.kbsc.domain.chat.Repository;
+
+import kbsc.kbsc.domain.chat.domain.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Integer> {
+    @Query()
+}
--- src/main/java/kbsc/kbsc/domain/chat/domain/ChatRoom.java
+++ src/main/java/kbsc/kbsc/domain/chat/domain/ChatRoom.java
@@ -2,27 +2,45 @@ package kbsc.kbsc.domain.chat.domain;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import kbsc.kbsc.domain.chat.application.ChatService;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.WebSocketSession;
 
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
 import java.util.*;
+//데이터 베이스 테이블과 1:1 맵핑되도록 구성
 
-@Slf4j
 @Getter
 @Setter
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "ChatRoom")
 public class ChatRoom {
-    private String roomId;
 
-    //private Integer postIdx;
-    private String lastMessage;
-    private Integer chaenumi;
-    private Integer nanumi;
-    private Set<WebSocketSession> sessions = new HashSet<>();
-    private List<ChatMessage> chatMessages = new ArrayList<>();
+    //pk
+    @Id
+    Integer chatRoomIdx;
+
+    Integer postIdx;
+    Integer nanumiIdx;
+    String lastMessage;
+    Timestamp updatedAt;
+    Integer chaenumiIdx;
+    Timestamp createdAt;
+
+    public void update(String msg) {
+        this.lastMessage = msg;
+        updatedAt = new Timestamp(System.currentTimeMillis());
+    }
+    /*
+    Set<WebSocketSession> sessions = new HashSet<>();
+    List<ChatMessage> chatMessages = new ArrayList<>();
 
     @Builder
     public ChatRoom(String roomId, Integer chaenumi, Integer nanumi) {
@@ -44,6 +62,6 @@ public class ChatRoom {
     private <T> void sendMessage(T message, ChatService chatService) {
         sessions.parallelStream()
                 .forEach(session -> chatService.sendMessage(session, message));
-    }
+    }*/
 }
 
